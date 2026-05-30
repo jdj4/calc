aeeecac907e4bbc823e6048c6284dc1fa2d60b3a
jjj404 <jjaeger4@arizona.edu>
2026-05-30 12:43:02 -0700

    CLI frontend created

---
+++ calc.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define VERSION "calc 1.0.0"
+#define COPYRIGHT "Copyright (C) 2026 jdj4. All Rights Reserved."
+
+static void print_help_msg(void) {
+}
+
+static void print_welcome_msg(void) {
+    printf(VERSION);
+    puts(" by jdj4");
+    puts("Use CLI arg --help for usage information");
+}
+
+static void args(int argc, char *arg) {
+    if (argc > 1) {
+        if (strcmp(arg, "--version") == 0) {
+            puts(VERSION);
+            exit(0);
+        } else if (strcmp(arg, "--credits") == 0) {
+            puts(COPYRIGHT);
+            exit(0);
+        } else if (strcmp(arg, "--help") == 0) {
+            print_help_msg();
+            exit(0);
+        } else if (strcmp(arg, "-q") == 0) {
+        } else print_welcome_msg();
+    } else print_welcome_msg();
+}
+
+int main(int argc, char *argv[]) {
+    args(argc, argv[1]);    
+    size_t n = 0;
+    char *line = NULL;
+    while (!feof(stdin)) {
+        printf(">>> ");
+        while (getline(&line, &n, stdin) > 0) {
+            printf("%s", line);
+            break;
+        }
+    }
+    return 0;
+}
